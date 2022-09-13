af2c912692a26cc6a7e0b3a580bb273a01d712c5
王晓睿 <wangxiaorui@zto.com>
2022-09-13 21:45:16 +0800

    leetcode2

---
+++ LeetCode2/LeetCode2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCode2
+//
+//  Created by 王晓睿 on 2022/9/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
