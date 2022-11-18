a92aa9fc736de6313763d27022ec8d9176d4c30f
wangxiaorui19 <wangxiaorui19@jd.com>
2022-11-18 19:46:46 +0800

    js

--- 前端/h5Demo/home.js
+++ 前端/h5Demo/home.js
@@ -11,3 +11,38 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 });
+
+class Person {
+    name = '';
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    introduceSelf(){
+        alert(`HI, My name is ${this.name}`);
+    }
+}
+
+const mike = new Person('Mike');
+mike.introduceSelf();
+
+
+class Teacher extends Person {
+    work = '';
+    constructor(name, work) {
+        super(name);
+        this.work = work;
+    }
+    introduceSelf(){
+        alert(`HI, My name is ${this.name}, and my work is teach ${this.work}`);
+    }
+    grade(paper) {
+        const grade = Math.floor(Math.random() * (5 - 1) + 1);
+        console.log(grade);
+    }
+}
+
+const teacher = new Teacher('ma', 'math');
+teacher.introduceSelf();
+teacher.grade(90);
