37665b50d45befe8061af000b3c8d58dfb8d16cb
wangxiaorui19 <wangxiaorui19@jd.com>
2022-11-23 18:59:29 +0800

    异步函数

--- 前端/h5Demo/home.js
+++ 前端/h5Demo/home.js
@@ -46,3 +46,134 @@ class Teacher extends Person {
 const teacher = new Teacher('ma', 'math');
 teacher.introduceSelf();
 teacher.grade(90);
+
+
+var superHeroes = {
+    "squadName" : "Super hero squad",
+    "homeTown" : "Metro City",
+    "formed" : 2016,
+    "secretBase" : "Super tower",
+    "active" : true,
+    "members" : [
+      {
+        "name" : "Molecule Man",
+        "age" : 29,
+        "secretIdentity" : "Dan Jukes",
+        "powers" : [
+          "Radiation resistance",
+          "Turning tiny",
+          "Radiation blast"
+        ]
+      },
+      {
+        "name" : "Madame Uppercut",
+        "age" : 39,
+        "secretIdentity" : "Jane Wilson",
+        "powers" : [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name" : "Eternal Flame",
+        "age" : 1000000,
+        "secretIdentity" : "Unknown",
+        "powers" : [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel"
+        ]
+      }
+    ]
+  };
+
+  superHeroes.homeTown = "aa";
+  superHeroes["members"][0].name
+  superHeroes.members[1].name
+
+/*
+const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/product.json');
+  console.log(fetchPromise);
+  fetchPromise.then(response => {
+    if(!response.ok){
+      throw new Error(`http error: ${response.status}`);
+    }
+    function headersForEach(value, key, parent){
+      console.log(key + ' = ' + '"' + value + '"');
+    }
+    console.log(`已经收到响应${response}`);
+    response.headers.forEach(headersForEach);
+    return response.json();
+  }).then(jsonObj => {
+    console.log(jsonObj);
+  }).catch(error => {
+    console.error(`捕获到error：${error}`);
+  });
+  console.log("已发送请求……");
+*/
+
+// async function fetchProducts() {
+//   try{
+//     const reponse = await fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+//     if(!reponse.ok) {
+//       throw new Error(`HTTP 请求错误：${reponse.status}`);
+//     }
+//     const json = await reponse.json();
+//     console.log(json[0].name);
+//     return json;
+//   }
+//   catch(error) {
+//     console.error(`无法获取产品列表：${error}`);
+//   }
+// }
+// const json = await fetchProducts(); // 这个json不是promise
+// const json = fetchProducts(); // 这个json是promise
+// console.log(json);
+
+
+async function testFunc(){
+  console.log('aaa func inner');
+  return "aaa";
+}
+
+console.log('aaa func before');
+// const aaa = await testFunc();
+console.log('aaa func after');
+
+async function testFunc2(){
+  const aaa = await testFunc();
+  return aaa;
+} 
+
+const aaa2 = testFunc2();
+aaa2
+
+const myFun = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // resolve(1)
+      reject(2)
+    },1000)
+  })
+  // return "aaaa";
+}
+myFun().then(aaa => {
+  console.log(`aaa: ${aaa}`);
+}).catch(bbb => {
+  console.log(`bbb: ${bbb}`);
+});
+
+const myFun2 = async() => {
+  try {
+    const res = await myFun();
+  console.log(`res: ${res}`);
+  } catch (error) {
+    console.log(`error: ${error}`);
+  }
+  
+}
+myFun2();
+
