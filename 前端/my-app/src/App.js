afbf022d8144ac68544476b25308f96a28335061
wangxiaorui19 <wangxiaorui19@jd.com>
2023-01-05 17:39:20 +0800

    feat: Refs & Dom

--- 前端/my-app/src/App.js
+++ 前端/my-app/src/App.js
@@ -84,9 +84,82 @@ class ThemeBox extends React.Component {
 
   render(){
     return (
-      <TypeContext.Provider value={this.state}>
+      <div>
+        <TypeContext.Provider value={this.state}>
         <ToolBar />
-      </TypeContext.Provider>
+        </TypeContext.Provider>
+        <CustomTextInput />
+        <AutoFocusTextInput />
+      </div>
+    );
+  }
+}
+
+/*
+class CustomTextInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.textInput = React.createRef();
+    this.focusTextInput = this.focusTextInput.bind(this);
+  }
+
+  focusTextInput() {
+    this.textInput.current.focus();
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.textInput}/>
+        <input type="button" value="Focus the text input" onClick={this.focusTextInput}/>
+      </div>
+    );
+  }
+}
+*/
+
+// ref传递一个函数
+class CustomTextInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.textInput = null;
+    this.setTextInputRef = element => {
+      this.textInput = element;
+    };
+    this.focusTextInput = () => {
+      if(this.textInput) this.textInput.focus();
+    };
+  }
+
+  componentDidMount() {
+    this.focusTextInput();
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.setTextInputRef} />
+        <input type="button" value="Focus the text input" onClick={this.focusTextInput}/>
+      </div>
+    );
+  }
+}
+
+class AutoFocusTextInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+  }
+
+  componentDidMount(){
+    this.textInput.current.focusTextInput();
+  }
+
+  render() {
+    return (
+      <CustomTextInput ref={this.textInput}/>
     );
   }
 }
