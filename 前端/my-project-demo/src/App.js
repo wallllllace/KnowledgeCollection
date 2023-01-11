68e39f90454334064485ed248ada20b37ed9f150
wangxiaorui19 <wangxiaorui19@jd.com>
2023-01-11 17:35:44 +0800

    feat: create project

---
+++ 前端/my-project-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world</h1>
+                <p>这是内容</p>
+            </div>
+        );
+    }
+}
+
+export default App;
