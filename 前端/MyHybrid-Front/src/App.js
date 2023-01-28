56afc1d218874890ca813aa01439fb6763342203
wangxiaorui19 <wangxiaorui19@jd.com>
2023-01-28 17:34:01 +0800

    feat: 仿写hybrid-cms前端

---
+++ 前端/MyHybrid-Front/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>hello world</h1>
+                <p>这就是内容</p>
+            </div>
+        );
+    }
+}
+
+export default App;
+
---
+++ 前端/MyHybrid-Front/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.[hash:6].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    mode: 'development', // 设置mode环境
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resouce',
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'MyHybrid',
+            minify: {
+                removeAttributeQuotes: true, // 移除属性的引号
+                removeComments: true, // 移除HTML中的注释
+                collapseWhitespace: true, // 删除空白符与换行符
+                minifyCSS: true // 压缩内联css
+            },
+            templateParameters: (compilation, assets, assetTags, options) => {
+                console.log(arguments);
+                return {
+                    title: "MyHybrid",
+                    rootId: "app"
+                };
+            },
+            inject: true,
+            hash: true,
+            filename: "index.html",
+            template: "./public/index.html"
+        }),
+    ]
+};
