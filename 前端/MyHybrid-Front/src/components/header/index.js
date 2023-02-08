79d11025aaad4f15ffefa3f2892c5ad796a6b976
wangxiaorui19 <wangxiaorui19@jd.com>
2023-02-08 18:53:15 +0800

    feat: css样式

--- 前端/MyHybrid-Front/src/components/header/index.js
+++ 前端/MyHybrid-Front/src/components/header/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react-lite";
 import { useHistory } from "react-router-dom";
-import "./style.scss";
+// import "./style.scss";
+import "./index.scss";
 import { navs } from "../../routers/navs";
+import logo from './hybrid-logo.svg';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     const history = useHistory();
@@ -29,11 +32,23 @@ const Header = () => {
         );
     };
 
+    const Notice = () => {
+        return (
+            <Marquee pauseOnHover gradient={false} gradientColor={[255, 255, 25]}>
+                我是公告
+            </Marquee>
+        );
+    }
+
     return (
         <div>
             <div className="header">
+                <img className="header-logo" src={logo}></img>
                 <div className="header-router-container">
                     <Tabs/>
+                </div>
+                <div className="header-notice-container">
+                    <Notice />
                 </div>
             </div>
             <div className="header-placement"></div>
--- 前端/MyHybrid-Front/webpack.config.js
+++ 前端/MyHybrid-Front/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resouce',
+                type:'asset/resource'
             }
         ]
     },
