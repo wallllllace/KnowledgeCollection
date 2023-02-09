eaba6e9b03bd20656f7fa4c2eb9b898eeb1742ad
wangxiaorui19 <wangxiaorui19@jd.com>
2023-02-09 17:37:56 +0800

    feat: header 完成

--- 前端/MyHybrid-Front/src/components/header/index.js
+++ 前端/MyHybrid-Front/src/components/header/index.js
@@ -6,6 +6,8 @@ import "./index.scss";
 import { navs } from "../../routers/navs";
 import logo from './hybrid-logo.svg';
 import Marquee from "react-fast-marquee";
+import { SoundOutlined } from "@ant-design/icons"; 
+import { notification, Select, Dropdown, Button } from 'antd'; 
 
 const Header = () => {
     const history = useHistory();
@@ -15,6 +17,16 @@ const Header = () => {
         setFocus(item.key);
         history.push(item.path);
     };
+
+    const openNotification = (title, body) => {
+        notification.open({
+          message: title,
+          description: body,
+          onClick: () => {
+            console.log('Notification Clicked!');
+          },
+        });
+      };
 
     const Tabs = () => {
         return (
@@ -34,11 +46,67 @@ const Header = () => {
 
     const Notice = () => {
         return (
+            <>
+            <SoundOutlined className="header-notice-container-SoundOutlined" />
             <Marquee pauseOnHover gradient={false} gradientColor={[255, 255, 25]}>
                 我是公告
             </Marquee>
+            </>
         );
-    }
+    };
+
+    const APPSelector = () => {
+        return (
+            <>
+            <Select
+            defaultValue="jd"
+            style={{ width: 120 }}
+            onChange={(e) => {
+                console.log(`选择了${e}`);
+            }}
+            options={[
+                { value: 'jd', label: '京东主站' },
+                { value: 'yhd', label: '一号店' },
+                { value: 'jx', label: '京喜' },
+                { value: 'jdjk', label: '京东健康' },
+            ]}/>
+            </>
+        );
+    };
+
+    const UserAccount = () => {
+        const items = [
+            {
+              key: '1',
+              label: (
+                <Button type="text" onClick={(e) => {
+                    console.log('个人资料');
+                }}>个人资料</Button> 
+              )
+            },
+            {
+              key: '2',
+              label: (
+                <Button type="text" onClick={(e) => {
+                    console.log('退出登录');
+                }}>退出登录</Button>  
+              )
+            }
+          ];
+          const onSelect = (item, key) => {
+            console.log(item + key);
+          };
+        return (
+            <>
+             <Dropdown menu={{items}} arrow >
+                <a className="header-userAccount-container-menu" onClick={(e) => e.preventDefault()}>
+                    wangxiaorui19
+                </a>
+            </Dropdown>
+            </>
+            
+        );
+    };
 
     return (
         <div>
@@ -47,8 +115,19 @@ const Header = () => {
                 <div className="header-router-container">
                     <Tabs/>
                 </div>
-                <div className="header-notice-container">
+                <div className="header-notice-container" onClick={(e) => {
+                    console.log('点击了公告');
+                    openNotification('公告', '我是公告内容');
+                }}>
                     <Notice />
+                </div>
+                <div className="header-right-container">
+                    <div className="header-appselector-container">
+                        <APPSelector />
+                    </div>
+                    <div className="header-userAccount-container">
+                        <UserAccount />
+                    </div>
                 </div>
             </div>
             <div className="header-placement"></div>
